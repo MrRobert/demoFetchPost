6ff485ba37b7e09467a5cec97f8d2abf498a8e8b
cuong.nd <cuong.nd@sutrixmedia.com>
2015-08-14 17:18:18 +0700

    init project

--- FetchingTool.php
+++ FetchingTool.php
@@ -47,11 +47,22 @@ class FetchingTool {
         }
 
         foreach($result['listSubCate'] as $cate){
+            if(sizeof($cate['subCate']) > 0){
+                // childCate cap 1 --------------------------
+                foreach($cate['subCate'] as $childCate){
+                    $dataChildCate = $this->fetchPostSingleCate($childCate['href']);
 
+                }
+            }
         }
     }
 
-    protected function fecthPostSingleCate($url){
+    /**
+     * @param $url - single cate url
+     * return list posts belong this cate
+     * @return array
+     */
+    protected function fetchPostSingleCate($url){
         $result = array();
         $dom = new DOMDocument('1.0');
         @$dom->loadHTMLFile($url);
@@ -59,12 +70,68 @@ class FetchingTool {
 
         $hostPost = $finder->query("//div[@id='box_news_top']//div[@class='block_news_big']//a[@class='txt_link']");
         $subHostPost = $finder->query("//div[@id='box_sub_hot_news']//div[@class='content_scoller width_common']//ul//li");
-        $secondPostLis = $finder->query("//div[@id='col_1']//ul[@id='news_home']//li");
-
+        if($hostPost != null){
+            array_push($result['lstPostContent'], array(
+                $this->fetchPost($hostPost[0]->nodeValue)
+            ));
+        }
+        if($subHostPost != null && sizeof($subHostPost) > 0){
+            foreach($subHostPost as $hostPost){
+                array_push($result['lstPostContent'], array(
+                    $this->fetchPost($hostPost->nodeValue)
+                ));
+            }
+        }
+        $secondPostLis = $finder->query("//div[@id='col_1']//ul[@id='news_home']//li//div[@class='title_news']//a[txt_link]");
+        if($secondPostLis != null && sizeof($secondPostLis) > 0){
+            foreach($secondPostLis as $post){
+                array_push($result['lstPostContent'], array(
+                    $this->fetchPost($post->getAttribute('href'))
+                ));
+            }
+        }
     }
 
-    private function fetchSingleCate($url){
+    /**
+     * @param $url
+     * get data content from 1 post
+     */
+    private function fetchPost($url){
+        $result = array();
+        $dom = new DOMDocument('1.0');
+        @$dom->loadHTMLFile($url);
+        $finder = new DOMXPath($dom);
+        $contentNode = $finder->query("//div[@id='box_details_news']//div[@id='left_calculator']");
 
+        // handle raw content for better
+        $rawContent = $contentNode->item(0)->nodeValue;
+
+        // find title
+        $titleNode = $finder->query("//div[@id='box_details_news']//div[@id='left_calculator']//div[@class='title_news']")->item(0);
+        $title = $titleNode->nodeValue;
+
+        // remove relate posts zone
+        // TODO : more code here
+
+
+        // check time ís in a week ( is acceptable ?)
+        // TODO :
+
+
+        // remove post time
+        // TODO: more code here
+
+
+        // remove social plugin zone
+        // TODO: more code here
+
+
+        return array(
+            'title' => $title,
+            'href' => $url,
+            'img' => 'img',
+            'content' => 'content'
+        );
     }
 
     private function isPostTimeAcceptable(){
